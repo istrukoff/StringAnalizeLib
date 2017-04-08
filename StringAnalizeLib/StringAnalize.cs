b82f7a33a6607a4f4224e85ed2e1d6d156e1381f
Igor Strukov <istrukoff@yandex.ru>
2017-04-08 13:32:14 +0500

    Init Commit.

---
+++ StringAnalizeLib/StringAnalize.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAnalizeLib
+{
+    public class StringAnalize
+    {
+        public StringAnalize() { }
+
+        public StringAnalize(string p_input)
+        {
+            input = p_input;
+        }
+
+        private string input;
+        public string Input { get { return input; } set { input = value; } }
+
+        // функция обработки { | }
+        private string parseBrace(string s)
+        {
+            Random rnd = new Random();
+            if (s.Contains('{'))
+            {
+                int clspos = s.IndexOf('}'); // ищем первую закрывающуюся скобку
+                int opnpos = clspos;
+                while (!s[opnpos].Equals('{')) // ищем соответствующую ей открывающуюся скобку
+                    opnpos--;
+                string block = s.Substring(opnpos, clspos - opnpos + 1); // берём подстроку между ними
+                string[] items = block.Substring(1, block.Length - 2).Split('|'); // разбиваем подстроку указанным символом на массив строк
+                s = s.Replace(block, items[rnd.Next(items.Length)]); // берём из полученного массива случайную строку и передаём её в результирующую строку
+                return parseBrace(s); // рекурсивно вызываем функцию
+            }
+            else
+                return s.Trim();
+        }
+
+        // перемешивание массива строк
+        private string randomizeString(string[] items)
+        {
+            string result = "";
+            Random rnd = new Random();
+            int c = items.Length;
+
+            for (int i = 0; i < c; i++)
+            {
+                int k = rnd.Next(c);
+                string m = items[k];
+                items[k] = items[i];
+                items[i] = m;
+            }
+
+            foreach (string t in items)
+                result += t + " ";
+
+            return result.Trim();
+        }
+
+        // функция обработки [ | ]
+        private string parseSquare(string s)
+        {
+            if (s.Contains('['))
+            {
+                int clspos = s.IndexOf(']'); // ищем первую закрывающуюся скобку
+                int opnpos = clspos;
+                while (!s[opnpos].Equals('[')) // ищем соответствующую ей открывающуюся скобку
+                    opnpos--;
+                string block = s.Substring(opnpos, clspos - opnpos + 1); // берём подстроку между ними
+                string[] items = block.Substring(1, block.Length - 2).Split('|'); // разбиваем подстроку указанным символом на массив строк
+                s = s.Replace(block, randomizeString(items)); // перемешиваем строки в полученном массиве и передаём их в результирующую строку
+                return parseSquare(s); // рекурсивно вызываем функцию
+            }
+            else
+                return s.Trim();
+        }
+
+        // функция обработки символов Unicode
+        private string parseUnicode(string s)
+        {
+            StringBuilder result = new StringBuilder();
+            UnicodeDictionary dictionary = new UnicodeDictionary();
+            dictionary.fillDictionary();
+            string[] bag = s.Split(' ');
+
+            foreach (string t in bag)
+                if (t.Contains("~"))
+                    result.Append(dictionary.getValue(t));
+                else
+                    result.Append((Array.IndexOf(bag, t) == 0) ? t : " " + t);
+
+            return result.ToString();
+        }
+
+        public string getParsedString()
+        {
+            string parse_brace = parseBrace(input);
+            string parse_square = parseSquare(parse_brace);
+            return parseUnicode(parse_square);
+        }
+    }
+}
---
+++ StringAnalizeLib/UnicodeDictionary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAnalizeLib
+{
+    public class UnicodeDictionary
+    {
+        public UnicodeDictionary() { }
+
+        private Dictionary<string, string> bags = new Dictionary<string, string>();
+
+        public Dictionary<string, string> fillDictionary()
+        {
+            bags.Add("~nl", "\u000A"); // перенос строки
+            bags.Add("~kross", "\uD83D\uDC5F"); // кроссовок
+            bags.Add("~phone", "\u260E"); // стационарный телефон
+            bags.Add("~mobilephone", "\uD83D\uDCF2"); // сотовый телефон
+            bags.Add("~office", "\uD83C\uDFE4"); // дом (офис)
+            bags.Add("~airplane", "\u2708"); // самолёт (1 вариант)
+            bags.Add("~airplane2", "\uD83D\uDEEB"); // самолёт (2 вариант)
+            bags.Add("~airplane3", "\uD83D\uDEEC"); // самолёт (3 вариант)
+            bags.Add("~airplane4", "\uD83D\uDEE9"); // самолёт (4 вариант)
+            bags.Add("~rocket", "\uD83D\uDE80"); // ракета
+            bags.Add("~euro", "\uD83D\uDCB6"); // купюра евро
+            bags.Add("~thumbsup", "\uD83D\uDC4D"); // палец вверх (лайк)
+            bags.Add("~fingertolink", "\uD83D\uDC47"); // указательный палец вниз
+            bags.Add("~smile", "\uD83D\uDE0A"); // улыбка
+            bags.Add("~heart", "\u2665"); // сердечко
+            bags.Add("~valentineheart", "\uD83D\uDC9D"); // сердечко в виде подарка
+            bags.Add("~kiss", "\uD83D\uDE17"); // поцелуй
+
+            return bags;
+        }
+
+        public string getValue(string key)
+        {
+            return (bags.ContainsKey(key)) ? bags[key] : " ";
+        }
+    }
+}
